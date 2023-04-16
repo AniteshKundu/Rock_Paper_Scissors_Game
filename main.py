52cdc9bf071e6a7aa3f40a4fa86511284fbba5b0
AniteshKundu <aniteshkundu.cse2020@nsec.ac.in>
2023-04-17 01:28:37 +0530

    Revised1 commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 def get_choices():
     player_choice = input("Enter a choice (Rock, Paper, Scissors) : ")
-    options = ["Rock", "Paper", "Scissors"]
+    options = ["Rock", "Paper", "Scissor"]
     computer_choice = random.choice(options)
     choises = {"player": player_choice , "computer": computer_choice}
     return choises
@@ -9,19 +9,19 @@ def check_win(player, computer ):
     print(f"You choose {player}, computer choose {computer}")
     if player == computer:
         return "It's a tie !"
-    elif player == "rock":
-        if computer == "scissor":
+    elif player == "Rock":
+        if computer == "Scissor":
             return "Rock smashes scissors ! You win !"
         else:
-            return "Peper cover rock ! You lose ."
-    elif player == "paper":
-        if computer == "rock":
-            return "Paper cover rock ! You win !"
+            return "Peper cover Rock ! You lose ."
+    elif player == "Paper":
+        if computer == "Rock":
+            return "Paper cover Rock ! You win !"
         else:
-            return "Scissors cuts paper ! You lose ."
-    elif player == "scissor":
-        if computer == "paper":
-            return "Scissors cuts paper ! You win !"
+            return "Scissors cuts Paper ! You lose ."
+    elif player == "Scissor":
+        if computer == "Paper":
+            return "Scissors cuts Paper ! You win !"
         else:
             return "Rock smashes Scissors ! You lose ."
         
